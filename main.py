f1aaae2e344e8cbe52de085baae03daff30cca37
小黄鱼鱼鱼 <60614367+xinghyfish@users.noreply.github.com>
2022-03-30 14:04:13 +0800

    取消手动配置等待时间参数，改为自动等待元素并捕获

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 import json
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 """
 @author: Xosmos
@@ -32,15 +34,21 @@ location_dict = {
 }
 # target url, redirect to: "https://my.suda.edu.cn/portal/_s2/anonymous_sy/main.psp"
 suda_url = "https://my.suda.edu.cn"
-
+MAX_WAIT_SEC = 10
 
 """1. log in"""
 driver.get(suda_url)
 driver.maximize_window()
 # Store iframe web element
-iframe = driver.find_element(By.CSS_SELECTOR, ".unLoginSd_application_iframe > iframe")
-# switch to selected iframe, else won't find button
-driver.switch_to.frame(iframe)
+try:
+    iframe = WebDriverWait(driver, MAX_WAIT_SEC).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".unLoginSd_application_iframe > iframe"))
+    )
+except TimeoutException:
+    print("LOAD ERROR!")
+    driver.close()
+else: # switch to selected iframe, else won't find button
+    driver.switch_to.frame(iframe)
 # Now click on button to enter log in page
 button = driver.find_element(by=By.CLASS_NAME, value="button")
 button.click()
@@ -61,18 +69,40 @@ button.click()
 """3. enter navigate list and find health info page entrance"""
 nav_list = driver.find_elements(by=By.CLASS_NAME, value="nav_list")
 nav_list[1].click() # enter app list
-time.sleep(2) # wait 2 sec
-driver.switch_to.frame("frameServer")   # IMPORTANT, else don't work
-service = driver.find_element(by=By.ID, value="1929")   # find health app
-service.click() # then a new page jumps out
-time.sleep(1) # wait
+try:
+    iframe = WebDriverWait(driver, MAX_WAIT_SEC).until(
+        EC.presence_of_element_located((By.ID, "frameServer"))
+    )
+except TimeoutException:
+    print("LOAD ERROR")
+    driver.close()
+else:
+    driver.switch_to.frame(iframe)   # IMPORTANT, else don't work
+
+try:
+    service = WebDriverWait(driver, MAX_WAIT_SEC).until(
+        EC.presence_of_element_located((By.ID, "1929"))
+    )   # find health app
+except TimeoutException:
+    print("LOAD ERROR")
+    driver.close()
+else:
+    service.click() # then a new page jumps out
 
 """4. switch to health-app and fill form"""
 windows = driver.window_handles
 driver.switch_to.window(windows[1]) # switch to new web to go on
-time.sleep(1)   # wait...
 # fill in body temperature, default 36
-driver.find_element(by=By.ID, value="input_swtw").send_keys(temperature)
+try:
+    morning_temp = WebDriverWait(driver, MAX_WAIT_SEC).until(
+        EC.presence_of_element_located((By.ID, "input_swtw"))
+    )
+    morning_temp.send_keys(temperature)
+except TimeoutException:
+    print("LOAD ERROR!")
+    driver.close()
+
+
 driver.find_element(by=By.ID, value="input_xwtw").send_keys(temperature)
 # roll page down
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -81,7 +111,6 @@ choices[2].click()
 # select current area
 selector = driver.find_element(by=By.XPATH, value="//*[@id=\"form\"]/div[3]/div[5]/div/div/div[2]/div/span/span[1]/span")
 selector.click()
-time.sleep(1)
 for i in range(location_dict[current_location]):
     selector.send_keys(Keys.DOWN)
 selector.send_keys(Keys.ENTER)
@@ -94,8 +123,16 @@ for i in range(15, 28, 2):
     choices[i].click()
 # post the form and stop
 driver.find_element(by=By.ID, value="tpost").click()
-time.sleep(2)
-driver.find_element(by=By.CLASS_NAME, value="layui-layer-btn0").click()
+
+try:
+    submit = WebDriverWait(driver, MAX_WAIT_SEC).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "layui-layer-btn0"))
+    )
+    submit.click()
+except TimeoutException:
+    print("LOAD ERROR!")
+finally:
+    driver.close()
 
 
 if __name__ == '__main__':
